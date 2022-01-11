c2f06ef18bf879a92999494b6c0368feaba928d3
Joris Hontelé <joris@tactics.be>
2022-01-11 21:26:21 +0100

    fixed shadow on categories screen

--- components/category-grid-tile/category-grid-tile.styles.js
+++ components/category-grid-tile/category-grid-tile.styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   gridItem: {
@@ -6,7 +6,11 @@ export default StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: 'hidden',
+    elevation: 5,
+    overflow:
+      Platform.OS === 'android' && Platform.Version >= 21
+        ? 'hidden'
+        : 'visible',
   },
   container: {
     flex: 1,
@@ -15,7 +19,6 @@ export default StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: 'flex-end',
     alignItems: 'flex-end',
